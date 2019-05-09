ebfb71c6a42edce712ce0e231fd0d95e04bfd3ce
Gurtej Sohi <gurtej.sohi@appdynamics.com>
2019-05-10 00:36:32 +0530

    requirement for injection using guice

--- src/main/java/com/gurtej/guice/SpellCheckerImpl.java
+++ src/main/java/com/gurtej/guice/SpellCheckerImpl.java
@@ -1,9 +1,20 @@
 package com.gurtej.guice;
 
+import com.google.inject.Inject;
+
 /**
- * Guice will use no args constructor for constructing an object of this class
+ * For injection using Guice:
+ * "Classes must have either one (and only one) constructor annotated with @Inject or a zero-argument constructor that is not private."
  */
 public class SpellCheckerImpl implements SpellChecker {
+
+    /**
+     * Guice will throw an error: look at it. To get rid of it, uncomment the below line
+     */
+//    @Inject
+    public SpellCheckerImpl(Integer x) {
+        System.out.println(x);
+    }
 
     @Override
     public void checkSpelling() {
